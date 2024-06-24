fefde25cfdd744ca74b8bcc94c72743f6c88bf17
hoareausylvie <hoareausms@gmail.com>
2024-06-24 12:43:26 +0400

    feat: création du fond de carte pour La Réunion

--- ReunionMap/map/views.py
+++ ReunionMap/map/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import PointOfInterest
+from django.core.serializers import serialize
 
 # Create your views here.
+def map_view(request):
+    points = PointOfInterest.objects.all()
+    points_json = serialize('json', points)
+    return render(request, 'map/map.html', {'points': points_json})
